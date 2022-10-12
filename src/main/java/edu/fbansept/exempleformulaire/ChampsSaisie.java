8e92c46898e87e7811f84b716c1755396903f992
fbansept <bansept.franck@gmail.com>
2022-10-12 15:55:21 +0200

    ajout textfield avec uniquement caractere

--- src/main/java/edu/fbansept/exempleformulaire/ChampsSaisie.java
+++ src/main/java/edu/fbansept/exempleformulaire/ChampsSaisie.java
@@ -1,15 +1,52 @@
 package edu.fbansept.exempleformulaire;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ChampsSaisie extends Box {
-    protected JTextField textField = new JTextField();
+
+    protected JTextField textField = new JTextField(){
+        @Override
+        protected void processKeyEvent(KeyEvent e) {
+
+            //Si il supprime ou utilise les fleches on laisse le traitement standard
+            if(e.getKeyCode() == KeyEvent.VK_RIGHT
+                    || e.getKeyCode() == KeyEvent.VK_LEFT
+                    || e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+                super.processKeyEvent(e);
+            } else {
+                //on créait l'expression régulière (n'importe quel caractère ou espace)
+                Pattern regex = Pattern.compile("[\\p{L}\s]");
+                //on transforme le caractère saisi en chaine de texte
+                String lettre = String.valueOf(e.getKeyChar());
+                //on créait un objet matcher à partire de la regex et la lettre saisie
+                Matcher matcher = regex.matcher(lettre);
+                //on vérifie si la lettre correspond à la regex
+                if (matcher.matches()) {
+                    super.processKeyEvent(e);
+                }
+            }
+        }
+    };
     protected JLabel icone = new JLabel();
     protected JLabel message = new JLabel();
 
     protected Border borderOriginal;
+
+    protected ImageIcon checkIcon;
+    protected ImageIcon errorIcon;
 
     public ChampsSaisie() {
         super(BoxLayout.Y_AXIS);
@@ -25,17 +62,46 @@ public class ChampsSaisie extends Box {
         add(ligne2);
 
         borderOriginal = textField.getBorder();
+
+        try {
+            checkIcon = new ImageIcon(
+                ImageIO.read(new File("src/main/resources/icones/check.png"))
+            );
+            errorIcon = new ImageIcon(
+                ImageIO.read(new File("src/main/resources/icones/error.png"))
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        textField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyChar() == 'a' || e.getKeyChar() == 'b') {
+                    System.out.println("ok");
+                }
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+                resetMessage();
+            }
+        });
     }
 
     public void erreur(String texte) {
         textField.setBorder(BorderFactory.createLineBorder(Color.red));
         message.setForeground(Color.RED);
         message.setText(texte);
+        icone.setIcon(errorIcon);
     }
 
     public void resetMessage() {
         textField.setBorder(borderOriginal);
         message.setText("");
+        icone.setIcon(null);
     }
 
     public String getText() {
